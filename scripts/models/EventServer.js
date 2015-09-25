3f06512829867ec22dcd161a419f8fe314ad6535
Pouja Nikray <poujanik@gmail.com>
2015-09-25 14:50:12 +0200

    Readded my own version of event emitter. Need to more control over the event emitting and listeners.

--- scripts/models/EventServer.js
+++ scripts/models/EventServer.js
@@ -1,7 +1,58 @@
-import {EventEmitter2} from 'eventemitter2';
+import _ from 'lodash';
 
-export default new EventEmitter2({
-    wildcard: true,
-    delimiter: '::',
-    maxListeners: 400
-});
+var listeners = {};
+var EventListener = {
+    flush(){
+        listeners = {};
+    },
+    /**
+     * Will call fn when 'name' event is emitted.
+     * @param  {String}   name The name of the event to listen on.
+     * @param  {Function} fn   The function to be invoked.
+     */
+    on(name, fn, id) {
+        if (!listeners.hasOwnProperty(name)) {
+            listeners[name] = [];
+        }
+        listeners[name].push({
+            id: id,
+            fn: fn
+        });
+    },
+    /**
+     * Emit the event
+     * @param  {String}    name   The name of the event.
+     * @param  {Array} values The values that should be sent with the emit.
+     */
+    emit(name, ...values) {
+        if(listeners.hasOwnProperty(name)){
+            listeners[name].forEach(function(fn){
+                // console.log(`invoke ${fn.id}::name`);
+                setTimeout(function(){
+                    fn.fn(...values);
+                })
+                // console.log(`done ${fn.id}::name`);
+            });
+        }
+    },
+    /**
+     * Partial call emit, this is usefull when you want to emit this when a dom event occurs.
+     * For example: onClick(EventListener.partialEmit('click', 'some', 'values')).
+     * @param  {String}    name   The name of the event.
+     * @param  {Array} values     The values that should be sent with the emit.
+     * @return {Function}         A partial function of EventListener.emit
+     */
+    partialEmit(name, ...values){
+        return _.partial(EventListener.emit, name, ...values);
+    }
+};
+
+export default EventListener;
+
+// import {EventEmitter2} from 'eventemitter2';
+
+// export default new EventEmitter2({
+//     wildcard: true,
+//     delimiter: '::',
+//     maxListeners: 400
+// });
