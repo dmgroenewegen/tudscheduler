df53e1e0cac56917c280e9c99ef034da6ca66f7f
Pouja Nikray <poujanik@gmail.com>
2016-07-14 10:30:57 +0200

    Added groups in the CSISPFields and basic.json
    Started working on ableling to restrict the courses that can be added to
    a certain field

--- src/constants/CSISPFields.js
+++ src/constants/CSISPFields.js
@@ -1,21 +1,34 @@
 // The fields for the ISP of the computer science master
+// group selection
+//      type: 'any' the course should have the group 'type'
+//      type: 'single' only one of this type in all fields can be picked and the course should have that selecte group type and value
+//      type: 'multiple' the type should be the same in the field, but the same type with another value can be used in another field
+//      type: value the course should have the type and value
 const fields = [{
     name: 'Compulsory Courses',
     minEC: 25,
-    minCourses: 5
-},{
+    minCourses: 5,
+    groups: {
+        track: 'single',
+        compulsory: true
+    }
+}, {
     name: 'Research Group',
-    minEC: 15
-},{
+    minEC: 15,
+    groups: {
+        'research-group': 'single'
+    }
+}, {
     name: 'Homolagation courses',
     maxEC: 15
-},{
+}, {
     name: 'Free electives',
     maxEC: 25
-},{
+}, {
     name: 'Seminar',
     minEC: 5,
     maxCourses: 1,
     minCourses: 1
 }];
-export default fields;
+export
+default fields;
