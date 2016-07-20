8bf7a53dc04874efad301cf483f81ff216a55dd9
Pouja Nikray <poujanik@gmail.com>
2016-07-20 13:24:26 +0200

    ISPModalOVerview updated with the changes in CourseCtrl
    ISPModalOverview was not updated with the recent changes to the course
    tree and how the courses are passed to each component.
    
    This resulted in an empty overview.

--- src/components/select/ISPModalOverview.js
+++ src/components/select/ISPModalOverview.js
@@ -1,13 +1,21 @@
-import React, {PropTypes} from 'react';
-import {Modal, Button, Table, Col} from 'react-bootstrap';
+import React, {
+    PropTypes
+}
+from 'react';
+import {
+    Modal, Button, Table, Col
+}
+from 'react-bootstrap';
 import ISPCtrl from '../../models/ISPCtrl.js';
+import CourseCtrl from '../../models/CourseCtrl.js';
 import _ from 'lodash';
 
 /**
  * Renders the modal form that shows all the isp fields with the selected courses and shows the user credentials.
  */
-export default React.createClass({
-    propTypes:{
+export
+default React.createClass({
+    propTypes: {
         data: PropTypes.object.isRequired,
         edit: PropTypes.func.isRequired,
         closeModal: PropTypes.func.isRequired,
@@ -17,32 +25,22 @@ export default React.createClass({
      * Sums all the ects of all the courses
      * @return {Number} The total ects of all the selected courses.
      */
-    totalEcts(){
-        return _(ISPCtrl.ispFields)
-            .filter(function(field){
+    totalEcts() {
+        return CourseCtrl.sumEcts(ISPCtrl.ispFieldModels
+            .filter(function(field) {
                 return field.getID() !== 'unlisted';
-            })
-            .map(function(field){
-                return field.getCourses();
-            })
-            .sumBy(this.subtotalEcts);
-    },
-    /**
-     * @param  {Array} courses A list of courses
-     * @return {Number} the ects of the given courses
-     */
-    subtotalEcts(courses){
-        return _.sumBy(courses, function(course){
-            return parseInt(course.ects, 10);
-        });
+            }).reduce(function(acc,field) {
+                return acc.concat(field.getCourses());
+            }, []));
     },
     /**
      * Renders a course, showing the course id, name and ects
-     * @param  {Object} course A course object
+     * @param  {Object} courseTree A course tree object
      * @param  {Number} index  The key of the element
      * @return {React}        A react component
      */
-    renderCourse(course, index){
+    renderCourse(courseTree, index) {
+        const course = CourseCtrl.get(courseTree.id);
         return <tr key={index}><td>{course.name}</td>
             <td>{course.courseName}</td><td>{course.ects}</td><td></td></tr>;
     },
@@ -52,13 +50,13 @@ export default React.createClass({
      * @param  {Number} index The key of the element.
      * @return {React}       A react component
      */
-    renderField(field, index){
+    renderField(field, index) {
         return (<tbody><tr key={index} className="header blue">
             <td colSpan="4">{field.getOptions().name}</td>
         </tr>{field.getCourses().map(this.renderCourse)}
         <tr>
             <td></td><td className="text-bold text-right">Subtotal</td>
-            <td>{this.subtotalEcts(field.getCourses())}</td>
+            <td>{CourseCtrl.sumEcts(field.getCourses())}</td>
             <td></td>
         </tr></tbody>);
     },
@@ -66,12 +64,12 @@ export default React.createClass({
      * Renders all the fields
      * @return {Array} An array of react components.
      */
-    renderFields(){
-        return ISPCtrl.ispFieldModels.filter(function(field){
+    renderFields() {
+        return ISPCtrl.ispFieldModels.filter(function(field) {
             return field.getID() !== 'unlisted';
         }).map(this.renderField);
     },
-    render(){
+    render() {
         return <Modal id="isp-form" className="print" show={this.props.show} bsSize="large"
         onHide={this.props.closeModal}>
             <Modal.Header closeButton>
